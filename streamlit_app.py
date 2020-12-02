2e41a336085e781affe8b4b1e2ecd19fcccdc2d5
yucongo <yucongo@gmail.com>
2020-12-02 15:01:18 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,10 @@ If you have any questions, checkout our [documentation](https://docs.streamlit.i
 forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
+xyz
 """
 
+st.wirte(" simpletransformers: ", simpletransformesr.__version__)
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
